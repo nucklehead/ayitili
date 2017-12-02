1ac52f6a63f72b88c54acc6964f67d14e8c44c23
nucklehead <pierevans@gmail.com>
2017-12-02 16:25:13 -0500

    Moved bean to diffrent class to solve circular dependencies

---
+++ src/main/java/com/colorofhope/ayitili/config/EncoderConfig.java
@@ -0,0 +1,15 @@
+package com.colorofhope.ayitili.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class EncoderConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }
+}
